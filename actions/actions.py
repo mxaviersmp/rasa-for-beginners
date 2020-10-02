28541d526f29e4b95522e254e5f7258adcbe4506
flych3r <xavier_sampaio@alu.ufc.br>
2020-10-01 22:35:13 -0300

    forms

--- actions/actions.py
+++ actions/actions.py
@@ -6,21 +6,52 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Dict, EventType, List, Text, Union
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+
+
+class HealthForm(FormAction):
+
+    def name(self) -> Text:
+        return 'health_form'
+
+    @staticmethod
+    def required_slots(tracker: 'Tracker') -> List[Text]:
+        if tracker.get_slot('confirm_exercise'):
+            return ['confirm_exercise', 'exercise', 'sleep', 'diet', 'stress', 'goal']
+        else:
+            return ['confirm_exercise', 'sleep', 'diet', 'stress', 'goal']
+
+    def submit(
+        self,
+        dispatcher: 'CollectingDispatcher',
+        tracker: 'Tracker',
+        domain: Dict[Text, Any]
+    ) -> List[EventType]:
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        return {
+            'confirm_exercise': [
+                self.from_intent(intent='affirm', value=True),
+                self.from_intent(intent='deny', value=False),
+                self.from_intent(intent='inform', value=True),
+            ],
+            'sleep': [
+                self.from_entity(entity='sleep'),
+                self.from_intent(intent='deny', value=None),
+            ],
+            'diet': [
+                self.from_text(intent='inform'),
+                self.from_text(intent='deny'),
+                self.from_text(intent='affirm')
+            ],
+            'goal': [
+                self.from_text(intent='inform'),
+                self.from_text(intent='deny'),
+                self.from_text(intent='affirm')
+            ]
+        }
