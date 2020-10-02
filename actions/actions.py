31e688486012e909cdf49e597cec4b38790b459e
flych3r <xavier_sampaio@alu.ufc.br>
2020-10-02 12:02:04 -0300

    updated forms for rasa 2

--- actions/actions.py
+++ actions/actions.py
@@ -6,52 +6,96 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Dict, EventType, List, Text, Union
+import json
+import os
+from typing import Dict, List, Text
 
-from rasa_sdk import Tracker
+import requests
+from dotenv import load_dotenv
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import EventType, SlotSet
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+
+load_dotenv()
+
+AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+BASE_ID = os.getenv('BASE_ID')
+TABLE_NAME = os.getenv('TABLE_NAME')
 
 
-class HealthForm(FormAction):
+def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
+    request_url = 'https://api.airtable.com/v0/{}/{}'.format(BASE_ID, TABLE_NAME)
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer {}'.format(AIRTABLE_API_KEY)
+    }
+
+    data = {
+        'fields': {
+            'Exercised?': confirm_exercise,
+            'Type of exercise': exercise,
+            'Amount of sleep': sleep,
+            'Diet': diet,
+            'Stress': stress,
+            'Goal': goal,
+        }
+    }
+
+    try:
+        response = requests.post(
+            request_url, headers=headers, data=json.dumps(data)
+        )
+    except requests.exceptions.HTTPError as e:
+        raise SystemError(e)
+
+    print(response.status_code)
+    return response
+
+
+class ValidateHealthForm(Action):
 
     def name(self) -> Text:
-        return 'health_form'
+        return 'validate_health_form'
 
-    @staticmethod
-    def required_slots(tracker: 'Tracker') -> List[Text]:
-        if tracker.get_slot('confirm_exercise'):
-            return ['confirm_exercise', 'exercise', 'sleep', 'diet', 'stress', 'goal']
-        else:
-            return ['confirm_exercise', 'sleep', 'diet', 'stress', 'goal']
-
-    def submit(
-        self,
-        dispatcher: 'CollectingDispatcher',
-        tracker: 'Tracker',
-        domain: Dict[Text, Any]
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[EventType]:
-        return []
-
-    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
-        return {
-            'confirm_exercise': [
-                self.from_intent(intent='affirm', value=True),
-                self.from_intent(intent='deny', value=False),
-                self.from_intent(intent='inform', value=True),
-            ],
-            'sleep': [
-                self.from_entity(entity='sleep'),
-                self.from_intent(intent='deny', value=None),
-            ],
-            'diet': [
-                self.from_text(intent='inform'),
-                self.from_text(intent='deny'),
-                self.from_text(intent='affirm')
-            ],
-            'goal': [
-                self.from_text(intent='inform'),
-                self.from_text(intent='deny'),
-                self.from_text(intent='affirm')
+        print('ValidateHealthForm')
+        if tracker.get_slot('confirm_exercise'):
+            required_slots = [
+                'confirm_exercise', 'exercise', 'sleep', 'diet', 'stress', 'goal'
             ]
-        }
+        else:
+            required_slots = [
+                'confirm_exercise', 'sleep', 'diet', 'stress', 'goal'
+            ]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet('requested_slot', slot_name)]
+
+        # All slots are filled.
+        return [SlotSet('requested_slot', None)]
+
+
+class SubmitHealthForm(Action):
+
+    def name(self) -> Text:
+        return 'submit_health_form'
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        print('SubmitHealthForm')
+        response = create_health_log(
+            tracker.get_slot('confirm_exercise'),
+            tracker.get_slot('exercise'),
+            tracker.get_slot('sleep'),
+            tracker.get_slot('diet'),
+            tracker.get_slot('stress'),
+            tracker.get_slot('goal')
+        )
+        dispatcher.utter_message(template='utter_submit')
+        return []
